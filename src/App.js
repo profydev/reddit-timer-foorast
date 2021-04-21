264571914521fcc1ed397a7417560d717966df65
Forrest Walker <forrestpatwalker@hotmail.com>
2021-04-21 14:09:54 -0600

    feat: Add routes within the App.js
    Create the basic template for routes within the App.js
    file. This will be expanded upon later but for now a template
    will suffice.
    
    Resolves: #01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Search from './Components/Search';
+import './Style/normalize.css';
+import './Style/style.css';
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <BrowserRouter>
+        <Switch>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/">
+            App Placeholder
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
