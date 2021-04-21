29134961ee21267450d1951790d9eccfe3605332
Forrest Walker <forrestpatwalker@hotmail.com>
2021-04-21 14:58:57 -0600

    style: Refactor normalize.css to single location
    normalize.css did not need its own css file and instead could
    be imported in the style.css file within a single line.
    
    Resolves: #01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './Components/Search';
-import './Style/normalize.css';
 import './Style/style.css';
 
 function App() {
