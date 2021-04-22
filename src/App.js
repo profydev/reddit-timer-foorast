20deeb42b52f0f346eae6597a4c7fe313b00f4f7
Forrest Walker <forrestpatwalker@hotmail.com>
2021-04-22 14:09:40 -0600

    style: Add theme to App.js
    Create theme within the theme.js and imported
    it within the App.js and wrapped everything within
    ThemeProvider.
    
    Resolves: #01

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import Search from './Components/Search';
+import theme from './theme';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Normalize />
-        <GlobalStyle />
-        <Switch>
-          <Route path="/search"><Search /></Route>
-          <Route exact path="/">Home</Route>
-          <Route>404 - Not Found</Route>
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Normalize />
+          <GlobalStyle />
+          <Switch>
+            <Route path="/search"><Search /></Route>
+            <Route exact path="/">Home</Route>
+            <Route>404 - Not Found</Route>
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
